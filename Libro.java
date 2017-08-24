187c5a7c426978963a28f2473a1178a185d8e685
José Luis <chepemtz96@gmail.com>
2017-08-24 11:04:07 -0500

    Se le agrego el metodo de acceso "dimeDetalles", esto para regresarle toda la informacion del objeto libro al usuario.

--- Libro.java
+++ Libro.java
@@ -2,24 +2,41 @@ public class Libro
 {
     private String titulo;
     private String autor;
+    private int paginas;
     //constructor
-    public Libro(String tituloIni, String autorIni)
+    public Libro(String tituloIni, String autorIni, int nPaginas)
     {
         titulo=tituloIni;
         autor=autorIni;
+        paginas=nPaginas;
     }
     //Metodo de acceso
     public String dimeAutor()
     {
-        String cadAutor="";
-        cadAutor+=autor;
-        return cadAutor;
+        return autor;
     }
     
-        public String dimeTitulo()
+    public String dimeTitulo()
     {
-        String cadTitulo="";
-        cadTitulo+=titulo;
-        return cadTitulo;
+        return titulo;
+    }
+    public String dimePaginas()
+    {
+        String cadPaginas="";
+        cadPaginas+=paginas;
+        return cadPaginas;
+    }
+    public String dimeDetalles()
+    {
+        String cadDetalles="";
+        cadDetalles=cadDetalles+"Titulo: ";
+        cadDetalles+=titulo;
+        cadDetalles+=",";
+        cadDetalles+=" Autor: ";
+        cadDetalles+=autor;
+        cadDetalles+=",";
+        cadDetalles+=" Paginas: ";
+        cadDetalles+=paginas;
+        return cadDetalles;
     }
 }
