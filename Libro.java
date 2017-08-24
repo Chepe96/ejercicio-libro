c82f49d9f193f3c9617afe45a15f3ed9e4239b97
José Luis <chepemtz96@gmail.com>
2017-08-24 10:37:54 -0500

    Se le agregaron dos variables de instancia titulo y autor que registran estos respectivos datos para el libro

---
+++ Libro.java
@@ -0,0 +1,11 @@
+public class Libro
+{
+    private String titulo;
+    private String autor;
+    //constructor
+    public Libro(String tituloIni, String autorIni)
+    {
+        titulo=tituloIni;
+        autor=autorIni;
+    }
+}
