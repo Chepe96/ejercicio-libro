2ce45153b9788630195577dbbe73e8323da2e777
José Luis Fernando Martínez Martínez <jlfmtz_96@alumnos.uaslp.edu.mx>
2017-08-25 20:16:03 -0500

    Se agrego la variable "numero de referencia" y el metodo "cambia numero de referencia" tambien se modifico el metodo "dime detalles" para agregar el numero de referencia y checar la longitud de la cadena.

--- Libro.java
+++ Libro.java
@@ -3,12 +3,14 @@ public class Libro
     private String titulo;
     private String autor;
     private int paginas;
+    private String numeroDeRef;
     //constructor
     public Libro(String tituloIni, String autorIni, int nPaginas)
     {
         titulo=tituloIni;
         autor=autorIni;
         paginas=nPaginas;
+        numeroDeRef="";
     }
     //Metodo de acceso
     public String dimeAutor()
@@ -37,6 +39,23 @@ public class Libro
         cadDetalles+=",";
         cadDetalles+=" Paginas: ";
         cadDetalles+=paginas;
+        cadDetalles+=",";
+        cadDetalles+=" Numero de referencia: ";
+        if(numeroDeRef.length()<=0)
+        {
+            numeroDeRef+="ZZZ";
+        }
+        cadDetalles+=numeroDeRef;
         return cadDetalles;
+    }
+    
+    public void cambiaNumRef(String numRef)
+    {
+        numeroDeRef=numRef;
+    }
+    
+    public String dimeNumRef()
+    {
+        return numeroDeRef;
     }
 }
